0de39edf9bd5ae8fb17fd415e90555b0cfa3499c
Phillammon <alistair@hippotigris.org.uk>
2017-02-15 10:55:37 +0000

    Marking point one complete

---
+++ linefollowerbehaviours/src/linefollowerbehaviours/FindLineBack.java
@@ -0,0 +1,44 @@
+package linefollowerbehaviours;
+
+import lejos.nxt.LightSensor;
+import lejos.nxt.Motor;
+import lejos.robotics.navigation.DifferentialPilot;
+import lejos.robotics.subsumption.Behavior;
+import lejos.nxt.LCD;
+
+public class FindLineBack implements Behavior {
+	private boolean suppressed = false;
+	private DifferentialPilot pilot;
+	private LightSensor l1;
+	private LightSensor l2;
+	private CornerType corner;
+	private int threshhold = 440;
+	public FindLineBack(LightSensor l1, LightSensor l2, DifferentialPilot pilot, CornerType corner){
+		this.pilot = pilot;
+		this.l1 = l1;
+		this.l2 = l2;
+		this.corner = corner;
+	}
+	public boolean takeControl(){
+		return this.corner.timesincelineseen > 4000;
+	}
+	public void suppress() {
+		suppressed = true;
+	}
+	public void action() {
+		this.corner.turn = 0;
+        pilot.setTravelSpeed(75);
+        suppressed = false;
+    	pilot.steer(-200);
+        while (!suppressed && this.corner.timesincelineseen > 4000) {
+        	if (l1.getNormalizedLightValue() < threshhold || l2.getNormalizedLightValue() < threshhold){
+        		this.corner.timesincelineseen = 0;
+        	}
+        	this.corner.timesincelineseen++;
+        	this.corner.CT(l1.getNormalizedLightValue(), l2.getNormalizedLightValue());
+	    	LCD.drawInt(l1.getNormalizedLightValue(), 4, 0, 1);
+	    	LCD.drawInt(l2.getNormalizedLightValue(), 4, 0, 2);
+	    	LCD.drawString("Keeping Looking", 0, 3);
+        }
+	}
+}
